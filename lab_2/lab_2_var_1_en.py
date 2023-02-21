7b3d549e6fced02ac00e7f03cfff5201725a0994
Ruslan Agliullin <agliullin.sir.ruslan@gmail.com>
2023-02-21 11:14:52 +0400

    Update lab_2_var_1_en.py

--- lab_2/lab_2_var_1_en.py
+++ lab_2/lab_2_var_1_en.py
@@ -21,8 +21,6 @@ try:
         for j in a:
             res = re.findall(r'\b\d+\b', j) # находим все натуральные числа с учетом грамматики
             if len(res) == 1:
-#                print()
-#                res = re.split(';|,|\.|!|\?', res[0])
 #                print(res, len(res[0]), "|", j, len(j))
                 counter = 0         # счетчик для вывода
                 number_counter += 1
