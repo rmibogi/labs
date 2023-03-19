0bbcfd4d5a8e586c171b26451cec8da7e78b83ae
Ruslan Agliullin <agliullin.sir.ruslan@gmail.com>
2023-03-19 22:24:53 +0400

    lab_4 graphics v 1.0

--- lab_4/lab_4.py
+++ lab_4/lab_4.py
@@ -17,7 +17,11 @@
 #   B   C
 #   D   E
 
+import random
+import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
 
 def print_matrix(matrix):                  # функция вывода матрицы
     matrix1 = list(map(list, zip(*matrix)))
@@ -78,7 +82,67 @@ else:
 
 print("Отформатированная матрица F:")
 print_matrix(F)
-print()
 
 try:
-    if
+    if np.linalg.det(A) > sum(np.diagonal(F)):
+        print("Результат выражения A*AT – K * F:")
+        matrix_result = A*A.transpose() - F*k
+        print_matrix(matrix_result)
+    else:
+        G = np.tri(n)*A
+        print("Результат выражения (A^(-1) +G-F^(-1))*K:\n")
+        matrix_result = (np.linalg.inv(A)+G-np.linalg.inv(F))*k
+        print(matrix_result)
+
+except np.linalg.LinAlgError:
+    print("Одна из матриц является вырожденной (определитель равен 0), поэтому обратную матрицу найти невозможно.")
+
+print("Матрица, которая используется при построение графиков:\n", A)
+
+av = [np.mean(abs(A[i, ::])) for i in range(n)]
+av = int(sum(av))
+fig, axs = plt.subplots(2, 2, figsize=(11, 8))
+x = list(range(1, n+1))
+for j in range(n):
+    y = list(A[j, ::])
+
+    axs[0, 0].plot(x, y, ',-', label=f"{j} строка.")
+    axs[0, 0].set(title="График с использованием функции plot:", xlabel='Номер элемента в строке', ylabel='Значение элемента')
+    axs[0, 0].grid()
+
+    axs[0, 1].bar(x, y, 0.4, label=f"{j} строка.")
+    axs[0, 1].set(title="График с использованием функции bar:", xlabel='Номер элемента в строке', ylabel='Значение элемента')
+    if n <= 10:
+        axs[0, 1].legend(loc='lower right')
+        axs[0, 1].legend(loc='lower right')
+
+explode = [0]*(n-1)
+explode.append(0.1)
+sizes = [round(np.mean(abs(A[i, ::])) * 100/av, 1) for i in range(n)]
+axs[1, 0].set_title("График с ипользованием функции pie:")
+axs[1, 0].pie(sizes, labels=list(range(1, n+1)), explode=explode, autopct='%1.1f%%', shadow=True)
+
+def heatmap(data, row_labels, col_labels, ax, cbar_kw={}, **kwargs):
+    im = ax.imshow(data, **kwargs)
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    ax.set_xticks(np.arange(data.shape[1]), labels=col_labels)
+    ax.set_yticks(np.arange(data.shape[0]), labels=row_labels)
+    return im, cbar
+def annotate_heatmap(im, data = None, textcolors=("black","white"), threshold=0):
+    if not isinstance(data, (list, np.ndarray)):
+        data = im.get_array()
+    kw = dict(horizontalalignment="center", verticalalignment="center")
+    texts = []
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            kw.update(color=textcolors[int(data[i, j] > threshold)])
+            text = im.axes.text(j, i, data[i, j], **kw)
+            texts.append(text)
+    return texts
+im, cbar = heatmap(A, list(range(n)), list(range(n)), ax=axs[1, 1], cmap="magma_r")
+texts = annotate_heatmap(im)
+axs[1, 1].set(title="Создание аннотированных тепловых карт:", xlabel="Номер столбца", ylabel="Номер строки")
+plt.suptitle("Использование библиотеки matplotlib")
+plt.tight_layout()
+plt.show()
+
