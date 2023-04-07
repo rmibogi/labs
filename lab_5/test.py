26cd7c7e15dafc2e4e86f89a5c2a67711cffaa46
Ruslan Agliullin <agliullin.sir.ruslan@gmail.com>
2023-04-07 15:33:29 +0400

    lab 5 v 2.3

--- lab_5/test.py
+++ lab_5/test.py
@@ -15,15 +15,22 @@ def iterative_f(n):
         f0, f1, f2 = f1, f2, fn
     return fn if n > 2 else f2
 
+def iterative_f(n):
+    f0, f1, f2 = 1, 1, 3
+    for i in range(1, n + 1):
+        fn = 2 * f2 + f0
+        f0, f1, f2 = f1, f2, fn
+    return fn
+
 start = time.time()
-n = 2
+n = 4
 #print(recursive_f(n))
-#print(iterative_f(n))
+print(iterative_f(n))
 end = time.time()
 
 #print(end-start)
 
-fn = 1
+fn = 4
 
 def calculate_f(n):
     fn = [1] * (n + 1)
@@ -31,10 +38,19 @@ def calculate_f(n):
         fn[i] = 2 * fn[i-1] + fn[i-3]
     return fn[n]
 
-print(calculate_f(6))
+#print(calculate_f(6))
 
 def iterative_f(n):
     fn = [1] * (n + 1)
     for i in range(2, n + 1):
         fn[i] = 2 * fn[i-1] + fn[i-3]
     return fn[n]
+
+def iterative_f(n):
+    fn = [1] * 4
+    for i in range(2, n + 1):
+        fn[3] = 2 * fn[2] + fn[0]
+        fn[0], fn[1], fn[2] = fn[1], fn[2], fn[3]
+    return fn[3]
+
+print(iterative_f(5))
