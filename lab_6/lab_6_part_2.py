99e7a03a159b8ec786e48d50e1c9a1184838fcae
Ruslan Agliullin <agliullin.sir.ruslan@gmail.com>
2023-04-21 10:25:49 +0400

    lab 6 v 0.4

---
+++ lab_6/lab_6_part_2.py
@@ -0,0 +1,42 @@
+# Задание состоит из двух частей.
+# 1 часть – написать программу в соответствии со своим вариантом задания.
+# 2 часть – усложнить написанную программу, введя по своему усмотрению в условие минимум
+# одно ограничение на характеристики объектов и целевую функцию для оптимизации решения.
+# Гр. ИСТд-11
+# Вариант 1.Пароль состоит из К символов. Первые Т символов – латинские буквы, остальные - латинские буквы или цифры.
+# Обязательно наличие как минимум одной цифры. Все символы должны быть разные. Составьте все возможные пароли.
+
+def generate_passwords(K, T, password="", num_digits=0):
+    global count
+    if len(password) == K:
+        if num_digits >= 2:
+            print(password, end=" ")
+            count += 1
+        return
+    if len(password) < T:
+        for c in "abcdefghijklmnopqrstuvwxyz":
+            if c not in password:
+                generate_passwords(K, T, password + c, num_digits)
+    else:
+        for c in "abcdefghijklmnopqrstuvwxyz0123456789":
+            if c not in password:
+                if c.isdigit():
+                    generate_passwords(K, T, password + c, num_digits + 1)
+                else:
+                    generate_passwords(K, T, password + c, num_digits)
+
+count = 0
+
+K = int(input("Введите натуральное число K, количество символов из которых состоит пароль: \n"))
+while K <= 0:
+    K = int(input("Ошибка: введите натуральное число K: "))
+
+T = int(input("Введите натуральное число T, количество первых символов, которые будут латинскими буквами: \n"))
+while T <= 0 or T >= K:
+    T = int(input("Ошибка: введите натуральное число T, удовлетворяющее условию 0 < T < K: "))
+
+print("\nНайденные варианты паролей:")
+generate_passwords(K, T)
+
+print("\nКоличество найденных паролей:", count)
+
