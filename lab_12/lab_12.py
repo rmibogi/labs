409a81a94ef6e00281279bdf0d9525151cd0c8ad
Ruslan Agliullin <124513940+rmibogi@users.noreply.github.com>
2023-10-31 02:59:36 +0400

    lab 12 v 1.6

--- lab_12/lab_12.py
+++ lab_12/lab_12.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+from decimal import Decimal, getcontext
 
 def s_sum(x, t):
     n = 1
@@ -9,7 +10,7 @@ def s_sum(x, t):
     sign = 1
 
     while True:
-        curr_term = np.linalg.det(curr_x) / factorial
+        curr_term = Decimal(np.linalg.det(curr_x) / factorial)
         result += sign * curr_term
         n += 1
         sign = -sign
@@ -29,6 +30,8 @@ x = np.random.randint(0, 10, (k, k))
 
 print(x)
 print()
+
+getcontext().prec = t ** 10
 
 result = s_sum(x, t)
 print(f"Сумма ряда с точностью {t} знаков после запятой: {result:.{t}f}")
